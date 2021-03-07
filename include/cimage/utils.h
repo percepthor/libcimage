eaedff4f22079fff9c424894e62a7995401669b0
ermiry <erick.salas@ermiry.com>
2021-03-06 21:37:13 -0600

    Added base utils header

---
+++ include/cimage/utils.h
@@ -0,0 +1,7 @@
+#ifndef _CIMAGE_UTILS_H_
+#define _CIMAGE_UTILS_H_
+
+#define three_way_max(a, b, c) (a > b) ? ( (a > c) ? a : c) : ( (b > c) ? b : c)
+#define three_way_min(a, b, c) (a < b) ? ( (a < c) ? a : c) : ( (b < c) ? b : c)
+
+#endif
